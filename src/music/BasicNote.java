2b0929bf1fa1d0628b045330a708aac277608b7d
Brice Huang <bmhuang@mit.edu>
2016-10-22 16:02:20 -0400

    Fixed BasicNote TODO THINGS ARE BROKEN

--- src/music/BasicNote.java
+++ src/music/BasicNote.java
@@ -5,72 +5,54 @@ package music;
  */
 public class BasicNote{
     
+    private final Integer reducedNote;
     private final Integer reducedPitch;
-    private final Key key;
-    private final Integer accidental;
+    
+    private static final Integer[] NATURAL_REDUCED_PITCHES = {0,2,4,5,7,9,11};
+    private static final Character[] PITCH_NAMES = {'C','D','E','F','G','A','B'};
     
     /*
      * Abstraction Function: 
-     * reducedPitch is the number of semitones the note is above a C.  
-     * accidental is the number of sharps (+) or flats (-) applied to the note.  
-     * key is the 
+     * reducedNote is the number of notes the note is above a C (0-6)  
+     * reducedPitch is the number of semitones the note is above/below a C
+     * natural.  This can be not in the range 0-11: for example, Cb is 
+     * -1, B# is 12.  reducedPitch is such that this not has at most 2 
+     * sharps/flats
      * 
      * Rep Invariant: 
-     * 0<= reducedPitch < 12
-     * reducedPitch - accidental is in the key
+     * 0 <= reducedNote < 7
+     * reducedPitch is such that this note has at most 2 sharts/flats 
      * 
      * Rep Exposure:
      * only returns primitives and immutables
      */
 
     private void checkRep(){
-        assert(0 <= reducedPitch);
-        assert(reducedPitch < 12);
-        int reducedPitchBaseNote = (reducedPitch-accidental+ Key.SEMITONES_IN_OCTAVE) 
-                % Key.SEMITONES_IN_OCTAVE;
-        assert(key.isReducedPitchInKey(reducedPitchBaseNote));
+        assert(reducedNote >= 0);
+        assert(reducedNote < 7);
+        int accidental = reducedPitch - NATURAL_REDUCED_PITCHES[reducedNote];
+        assert(-2 <= accidental);
+        assert(accidental <= 2);
     }
 
     /**
-     * Constructs a BasicNote.  Not preferred constructor.   
-     * @param c the note's letter value 
-     * @param key the key in which this note is played
-     * @param accidental number of semitones to shift the note up or down, relative
-     * to the default note with this letter value in the key.  
+     * Constructor for BasicNotes
+     * @param reducedNote reduced note
+     * @param reducedPitch reduced pitch
      */
-    public BasicNote(char c, Key key, int accidental){
-        this.reducedPitch = key.getReducedPitchOfLetterNote(c, accidental);
-        this.accidental = accidental;
-        this.key = key;
-        checkRep();
-    }
-
-    /**
-     * Constructs a BasicNote.  Not preferred constructor.  
-     * @param scaleDegree the scale degree in the key
-     * @param key the key in which this note is played
-     * @param accidental number of semitones to shift the note up or down, relative
-     * to the default note with this scale degree in the key.
-     */
-    public BasicNote(int scaleDegree, Key key, int accidental){
-        this.reducedPitch = key.getReducedPitchOfScaleDegree(scaleDegree, accidental);
-        this.accidental = accidental;
-        this.key = key;
-        checkRep();
-    }
-
-    /**
-     * Constructs a BasicNote.  This is the preferred constructor.  
-     * @param c the note's letter value 
-     * @param reducedPitch the note's reduced pitch
-     * @param key the key in which the note is played
-     */
-    public BasicNote(char c, int reducedPitch, Key key){
+    public BasicNote(int reducedNote, int reducedPitch){
+        this.reducedNote = reducedNote;
         this.reducedPitch = reducedPitch;
-        this.accidental = ((reducedPitch - key.getReducedPitchOfLetterNote(c, 0) 
-                + Key.SEMITONES_IN_OCTAVE + Key.SEMITONES_IN_HALF_OCTAVE) 
-                % key.SEMITONES_IN_OCTAVE) - Key.SEMITONES_IN_HALF_OCTAVE;
-        this.key = key;
+        checkRep();
+    }
+    
+    /**
+     * Gets the string representation of a note as it is written in a given key 
+     * @param key a key
+     * @return ''
+     */
+    public String renderInKey(Key key){
+        throw new RuntimeException("Unimplemented.");
     }
     
     /*********************
@@ -80,57 +62,56 @@ public class BasicNote{
     /**
      * @return reduced pitch
      */
+    public int getReducedNote(){
+        return reducedNote;
+    }
+
+    /**
+     * @return reduced pitch
+     */
     public int getReducedPitch(){
         return reducedPitch;
     }
 
-    /**
-     * @return key
-     */
-    public Key getKey(){
-        return key;
-    }
-
-    /**
-     * @return scale degree
-     */
-    public int getScaleDegree(){
-        return key.reducedPitchToScaleDegree(reducedPitch - accidental);
-    }
-    
-    /**
-     * @return scale degree
-     */
-    public String getWrittenName(){
-        return key.getWrittenNameOfReducedPitch(reducedPitch, accidental);
-    }
     
     /*******************
      * Object Contract *
      *******************/
     
     /**
-     * Two basicNotes are equal iff they have the same reduced pitch.  
+     * Two basicNotes are equal iff they have the same reduced pitch and note.  
      * @param object another object
      * @return whether this and object are equal.   
      */
     @Override public boolean equals(Object object){
         if (!(object instanceof BasicNote)){ return false; }
         BasicNote that = (BasicNote) object;
-        return (this.reducedPitch == that.reducedPitch && this.accidental == that.accidental
-                && this.key.equals(that.key));
+        return (this.reducedPitch == that.reducedPitch && this.reducedNote == that.reducedNote);
     }
 
     @Override public int hashCode(){
-        return reducedPitch + 7*accidental + 23*key.hashCode(); 
+        return reducedPitch + 7*reducedNote; 
     }
 
     /**
      * String representation of this note
-     * @return the name of this note, as spelled within the key
+     * @return the name of this note, as written in C Major
      */
     @Override public String toString(){
-        return getWrittenName();
+        String stringRep = "";
+        stringRep += PITCH_NAMES[reducedNote];
+        int accidental = reducedPitch - NATURAL_REDUCED_PITCHES[reducedNote];
+        if (accidental>0){
+            for (int i=0; i<accidental; i++){
+                stringRep += '+';
+            }
+        }
+        else if (accidental < 0){
+            for (int i=0; i<-accidental; i++){
+                stringRep += '-';
+            }
+        }
+        return stringRep;
     }
 
 }
