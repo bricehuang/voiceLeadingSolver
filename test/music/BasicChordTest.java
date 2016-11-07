ae93809022ae2d3895a1f31e75dcb2feda2a65e4
Brice Huang <bmhuang@mit.edu>
2016-11-07 02:31:05 -0500

    BasicChordTest improved

--- test/music/BasicChordTest.java
+++ test/music/BasicChordTest.java
@@ -10,38 +10,32 @@ public class BasicChordTest {
         assert false;
     }
 
+    private static final BasicNote C = new BasicNote(0,0);
+    private static final BasicNote D = new BasicNote(1,2);
+    private static final BasicNote E = new BasicNote(2,4);
+    private static final BasicNote Eb = new BasicNote(2,3);
+    private static final BasicNote F = new BasicNote(3,5);
+    private static final BasicNote G = new BasicNote(4,7);
+    private static final BasicNote B = new BasicNote(6,11);
+    
+    private static final BasicChord G_DOM_SEVEN = new BasicChord(
+            B, D, F, G, new PrimitiveChord(G,ChordType.DOM7, 0));
+    private static final BasicChord C_MAJ = new BasicChord(
+            C, C, E, C, new PrimitiveChord(C, ChordType.MAJ, 0));
+    private static final BasicChord C_MIN = new BasicChord(
+            C, C, Eb, C, new PrimitiveChord(C, ChordType.MIN, 0));
+    private static final Key C_MINOR = new Key(-3, false);
+    
     @Test
     public void toStringTest(){
-        assertEquals("[B|D|F|G]", 
-                new BasicChord(
-                        new BasicNote(6,11), new BasicNote(1,2), 
-                        new BasicNote(3,5), new BasicNote(4,7),  
-                        new PrimitiveChord(new BasicNote(4,7), 
-                                ChordType.DOM7, 0)).toString());
-        assertEquals("[C|C|E|C]", 
-                new BasicChord(
-                        new BasicNote(0,0), new BasicNote(0,0), 
-                        new BasicNote(2,4), new BasicNote(0,0),  
-                        new PrimitiveChord(new BasicNote(0,0), 
-                                ChordType.MAJ, 0)).toString());
+        assertEquals("[B|D|F|G]", G_DOM_SEVEN.toString());
+        assertEquals("[C|C|E|C]", C_MAJ.toString());
     }
 
     @Test
     public void renderInKeyTest(){
-        Key cMinor = new Key(-3, false);
-        
-        assertEquals("[B=|D|F|G]", 
-                new BasicChord(
-                        new BasicNote(6,11), new BasicNote(1,2), 
-                        new BasicNote(3,5), new BasicNote(4,7),  
-                        new PrimitiveChord(new BasicNote(4,7), 
-                                ChordType.DOM7, 0)).renderInKey(cMinor));
-        assertEquals("[C|C|E|C]", 
-                new BasicChord(
-                        new BasicNote(0,0), new BasicNote(0,0), 
-                        new BasicNote(2,3), new BasicNote(0,0),  
-                        new PrimitiveChord(new BasicNote(0,0), 
-                                ChordType.MIN, 0)).renderInKey(cMinor));
+        assertEquals("[B=|D|F|G]", G_DOM_SEVEN.renderInKey(C_MINOR));
+        assertEquals("[C|C|E|C]", C_MIN.renderInKey(C_MINOR));
     }
 
 
