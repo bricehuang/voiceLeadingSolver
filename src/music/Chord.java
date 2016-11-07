13fc6e07df96e65d38bb8616b893f0fba17a2183
Brice Huang <bmhuang@mit.edu>
2016-11-07 02:31:23 -0500

    Chords implemented and tested

--- src/music/Chord.java
+++ src/music/Chord.java
@@ -4,26 +4,43 @@ package music;
  * An immutable class representing a chord sung by four voices.  
  */
 public class Chord {
-    // TODO methods not complete
+    
+    private final Note soprano;
+    private final Note alto;
+    private final Note tenor;
+    private final Note bass;
+    private final BasicChord basicChord;
+        
+    /*
+     * Abstraction Function:
+     * represents a chord with soprano, alto, tenor, bass singing these notes
+     * basicChord represents this chord modulo octaves
+     * 
+     * Rep Invariant:
+     * soprano, alto, tenor, bass are same notes as corresponding notes in basicChord
+     * voices don't cross
+     * voices aren't more than an octave apart, except tenor/bass
+     * notes are in voice ranges shown below
+     * 
+     * Rep Exposure:
+     * returns only primitives and immutables 
+     */
     
     /*
      * Voice ranges:
      * Soprano: D4-G5
      * Alto: A3-D5
-     * Tenor: D3-F4
-     * Bass: F2-C4
+     * Tenor: D3-G4
+     * Bass: E2-C4
      */
-    
-    /*
-     * Abstraction Function:
-     * TODO
-     * 
-     * Rep Invariant:
-     * TODO
-     * 
-     * Rep Exposure:
-     * TODO
-     */
+    public static final Note SOPRANO_LOWER = new Note(new BasicNote(1,2), 4);
+    public static final Note SOPRANO_UPPER = new Note(new BasicNote(4,7), 5);
+    public static final Note ALTO_LOWER = new Note(new BasicNote(5,9), 3);
+    public static final Note ALTO_UPPER = new Note(new BasicNote(1,2), 5);
+    public static final Note TENOR_LOWER = new Note(new BasicNote(1,2), 3);
+    public static final Note TENOR_UPPER = new Note(new BasicNote(4,7), 4);
+    public static final Note BASS_LOWER = new Note(new BasicNote(2,4), 2);
+    public static final Note BASS_UPPER = new Note(new BasicNote(0,0), 4);
     
     /**
      * Constructs a chord
@@ -32,12 +49,35 @@ public class Chord {
      * @param tenor tenor note
      * @param bass bass note
      */
-    public Chord(Note soprano, Note alto, Note tenor, Note bass){
-        throw new RuntimeException("Unimplemented");
+    public Chord(Note soprano, Note alto, Note tenor, Note bass, BasicChord basicChord){
+        this.soprano = soprano;
+        this.alto = alto;
+        this.tenor = tenor; 
+        this.bass = bass;
+        this.basicChord = basicChord;
     }
     
     private void checkRep(){
-        throw new RuntimeException("Unimplemented");
+        assert(soprano.getBasicNote().equals(basicChord.getSoprano()));
+        assert(alto.getBasicNote().equals(basicChord.getAlto()));
+        assert(tenor.getBasicNote().equals(basicChord.getTenor()));
+        assert(bass.getBasicNote().equals(basicChord.getBass()));
+        
+        assert(soprano.getNoteID() >= alto.getNoteID());
+        assert(alto.getNoteID() >= tenor.getNoteID());
+        assert(tenor.getNoteID() >= bass.getNoteID());
+        
+        assert(soprano.getNoteID() <= alto.getNoteID() + Key.PITCHES_IN_SCALE);
+        assert(alto.getNoteID() <= tenor.getNoteID() + Key.PITCHES_IN_SCALE);
+        
+        assert(soprano.getNoteID() <= SOPRANO_UPPER.getNoteID());
+        assert(soprano.getNoteID() >= SOPRANO_LOWER.getNoteID());
+        assert(alto.getNoteID() <= ALTO_UPPER.getNoteID());
+        assert(alto.getNoteID() >= ALTO_LOWER.getNoteID());
+        assert(tenor.getNoteID() <= TENOR_UPPER.getNoteID());
+        assert(tenor.getNoteID() >= TENOR_LOWER.getNoteID());
+        assert(bass.getNoteID() <= BASS_UPPER.getNoteID());
+        assert(bass.getNoteID() >= BASS_LOWER.getNoteID());
     }
     
     /**
@@ -56,30 +96,51 @@ public class Chord {
      * @return the soprano note
      */
     public Note getSoprano(){
-        throw new RuntimeException("Unimplemented");
+        return soprano;
     }
 
     /**
      * @return the alto note
      */
     public Note getAlto(){
-        throw new RuntimeException("Unimplemented");
+        return alto;
     }
     
     /**
      * @return the tenor note
      */
     public Note getTenor(){
-        throw new RuntimeException("Unimplemented");
+        return tenor;
     }
     
     /**
      * @return the bass note
      */
     public Note getBass(){
-        throw new RuntimeException("Unimplemented");
+        return bass;
     }
     
+    /**
+     * @return the bass note
+     */
+    public BasicChord getBasicChord(){
+        return basicChord;
+    }
+    
+    /***************
+     * Computation *
+     ***************/
+
+    /**
+     * @param key 
+     * @return the notes of this chord, rendered in given key 
+     */
+    public String renderInKey(Key key){
+        return "["+soprano.renderInKey(key)+"|"+alto.renderInKey(key)+
+                "|"+tenor.renderInKey(key)+"|"+bass.renderInKey(key)+"]";
+    }
+
+
     /*******************
      * Object Contract *
      *******************/
@@ -90,11 +151,19 @@ public class Chord {
      * @return whether this and object are equal.   
      */
     @Override public boolean equals(Object object){
-        throw new RuntimeException("Unimplemented.");
+        if (!(object instanceof Chord)){return false;}
+        Chord that = (Chord) object;
+        return (this.soprano.equals(that.soprano) &&
+                this.alto.equals(that.alto) &&
+                this.tenor.equals(that.tenor) &&
+                this.bass.equals(that.bass) &&
+                this.basicChord.equals(that.basicChord));
     }
 
     @Override public int hashCode(){
-        throw new RuntimeException("Unimplemented.");
+        return soprano.hashCode() + alto.hashCode() +
+                tenor.hashCode() + bass.hashCode() +
+                basicChord.hashCode();
     }
 
     /**
@@ -102,6 +171,7 @@ public class Chord {
      * notation
      */
     @Override public String toString(){
-        throw new RuntimeException("Unimplemented.");
+        return "["+soprano.toString()+"|"+alto.toString()+
+                "|"+tenor.toString()+"|"+bass.toString()+"]";
     }
 }
---
+++ test/music/ChordTest.java
@@ -0,0 +1,53 @@
+package music;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ChordTest {
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false;
+    }
+    
+    private static final BasicNote C = new BasicNote(0,0);
+    private static final BasicNote D = new BasicNote(1,2);
+    private static final BasicNote E = new BasicNote(2,4);
+    private static final BasicNote Eb = new BasicNote(2,3);
+    private static final BasicNote F = new BasicNote(3,5);
+    private static final BasicNote G = new BasicNote(4,7);
+    private static final BasicNote B = new BasicNote(6,11);
+    
+    private static final BasicChord G_DOM_SEVEN = new BasicChord(
+            B, D, F, G, new PrimitiveChord(G,ChordType.DOM7, 0));
+    private static final Chord G_DOM_SEVEN_REALIZED = new Chord(
+            new Note(B,4), new Note(D, 4), new Note(F, 3), 
+            new Note(G, 2),  G_DOM_SEVEN);
+            
+    private static final BasicChord C_MAJ = new BasicChord(
+            C, C, E, C, new PrimitiveChord(C, ChordType.MAJ, 0));
+    private static final Chord C_MAJ_REALIZED = new Chord(
+            new Note(C, 5), new Note(C, 4), new Note(E, 3), 
+            new Note(C, 3),  C_MAJ);
+
+    private static final BasicChord C_MIN = new BasicChord(
+            C, C, Eb, C, new PrimitiveChord(C, ChordType.MIN, 0));
+    private static final Chord C_MIN_REALIZED = new Chord(
+            new Note(C, 5), new Note(C, 4), new Note(Eb, 3), 
+            new Note(C, 3),  C_MIN);
+    private static final Key C_MINOR = new Key(-3, false);
+
+    @Test
+    public void toStringTest(){
+        assertEquals("[B4|D4|F3|G2]", G_DOM_SEVEN_REALIZED.toString());
+        assertEquals("[C5|C4|E3|C3]", C_MAJ_REALIZED.toString());
+    }
+
+    @Test
+    public void renderInKeyTest(){
+        assertEquals("[B=4|D4|F3|G2]", G_DOM_SEVEN_REALIZED.renderInKey(C_MINOR));
+        assertEquals("[C5|C4|E3|C3]", C_MIN_REALIZED.renderInKey(C_MINOR));
+    }
+
+
+}
