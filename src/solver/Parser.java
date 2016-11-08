717e79157ed3a2f1fef45e1057b38a4ae9c0975f
Brice Huang <bmhuang@mit.edu>
2016-11-08 01:47:29 -0500

    minor edit

--- src/solver/Parser.java
+++ src/solver/Parser.java
@@ -1,7 +1,7 @@
 package solver;
 
 /**
- * A module that takes a string input and returns a list of chord types
+ * A module that takes a string input and returns a list of primitive chords
  */
 public class Parser {
     // TODO
