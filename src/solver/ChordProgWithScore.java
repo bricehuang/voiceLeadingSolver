6e088537e7af30ef1f7046abc2fa9bf62f5b7ed1
Brice Huang <bmhuang@mit.edu>
2016-11-10 00:35:08 -0500

    ChordProgWithScore

---
+++ src/solver/ChordProgWithScore.java
@@ -0,0 +1,54 @@
+package solver;
+
+import chords.ChordProgression;
+
+/**
+ * Immmutable class representing a chord progression with a score
+ */
+public class ChordProgWithScore{
+    private final ChordProgression chordProg;
+    private final Integer score; 
+    
+    ChordProgWithScore(ChordProgression chordProg, Integer score){
+        this.chordProg = chordProg;
+        this.score = score;
+    }
+    
+    /***********
+     * Getters *
+     ***********/
+    
+    /**
+     * @return chord progression 
+     */
+    public ChordProgression getChordProg(){
+        return chordProg;
+    }
+    
+    public Integer getScore(){
+        return score;
+    }
+    
+    /*******************
+     * Object Contract *
+     *******************/
+    
+    @Override
+    public boolean equals(Object other){
+        if (!(other instanceof ChordProgWithScore)){return false;}
+        ChordProgWithScore that = (ChordProgWithScore) other;
+        return (this.chordProg.equals(that.chordProg) 
+                && this.score.equals(that.score));
+    }
+    
+    @Override
+    public int hashCode(){
+        return this.chordProg.hashCode() + 317*this.score.hashCode();
+    }
+    
+    @Override
+    public String toString(){
+        return this.chordProg.toString() + "\nScore: "+ this.score;
+    }
+
+}
