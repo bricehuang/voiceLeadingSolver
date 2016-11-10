b32fdb8402e693a329691c7e4e71fa29c1210441
Brice Huang <bmhuang@mit.edu>
2016-11-10 18:06:52 -0500

    Added heuristics to scorer

--- src/solver/Scorer.java
+++ src/solver/Scorer.java
@@ -8,6 +8,37 @@ import music.Key;
  */
 public class Scorer {
     
+    /*
+     * Heuristics:
+     * 
+     * Individual chords: 
+     * 
+     * Doubling rules
+     * No voice overlapping 
+     * 
+     * 
+     * Transitions:
+     * 
+     * Small voice movement
+     * No Parallels
+     * No Directs
+     * No melodic aug/dim intervals
+     * No voice crossing
+     * Dom7 proper resolutions
+     * Dim7 proper resolutionss
+     * ii65 hold tonic
+     * 
+     * Last Transition:
+     * 
+     * Everything moves stepwise except bass
+     * PAC
+     * 
+     */
+    
+    /*********************************
+     * scoreChord and helper methods *
+     *********************************/
+    
     /**
      * Scores the badness of a chord 
      * @param chord current chord
@@ -17,6 +48,10 @@ public class Scorer {
     public static Integer scoreChord(Chord chord, Key key){
         throw new RuntimeException("Unimplemented.");
     }
+    
+    /**************************************
+     * scoreTransition and helper methods *
+     **************************************/
 
     /**
      * Scores the badness of a transition 
@@ -28,6 +63,10 @@ public class Scorer {
     public static Integer scoreTransition(Chord previous, Chord current, Key key){
         throw new RuntimeException("Unimplemented.");
     }
+    
+    /****************************************************
+     * scoreLastTransitionAdditional and helper methods *
+     ****************************************************/
     
     /**
      * Additional score for last transition 
