6b3b9c4a8aa63a4282089d96f895eaa179a331e4
Brice Huang <bmhuang@mit.edu>
2016-11-10 01:11:10 -0500

    BestLists implemented! Infrastructure for sequence ready to go

---
+++ src/solver/BestList.java
@@ -0,0 +1,113 @@
+package solver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import chords.Chord;
+import chords.ChordProgression;
+
+/**
+ * Mutable class; represents the best chord progressions 
+ * ending with a particular chord
+ */
+public class BestList{
+    
+    public static final int PROGRESSIONS_TO_TRACK = 3;
+    
+    private final Map<Chord, List<ChordProgWithScore>> content;
+    
+    /*
+     * Abstraction Function:
+     * content.get(chord) represents the best progressions (and scores) 
+     * ending in chord that have been inputted so far, in increasing 
+     * order of badness score.  
+     * 
+     * Rep invariant:
+     * content.get(chord) has between 1 and PROGRESSIONS_TO_TRACK progressions
+     * all progressions in content.get(chord) end with chord
+     * progressions in content.get(chord) decrease in score
+     * 
+     * Rep exposure:
+     * Returns immutables and unmodifiable views of lists only
+     */
+    
+    /**
+     * Constructs a BestList
+     */
+    public BestList(){
+        content = new HashMap<>();
+        checkRep();
+    }
+    
+    private void checkRep(){
+        for (Chord chord : content.keySet()){
+            assert(content.get(chord).size() <= PROGRESSIONS_TO_TRACK);
+            assert(content.get(chord).size() > 0);
+            List<ChordProgWithScore> progs = content.get(chord);
+            for (ChordProgWithScore prog : progs){
+                assert(prog.getChordProg().getLast().equals(chord));
+            }
+            for (int i=1; i<progs.size(); i++){
+                assert(progs.get(i-1).getScore() <= progs.get(i).getScore());
+            }
+        }
+    }
+    
+    /**
+     * Returns the best chord progressions ending with a particular chord
+     * @param chord a chord
+     * @return an unmodifiable view of a list of the best chord 
+     * progressions ending with this chord, in increasing order of 
+     * badness score 
+     */
+    public List<ChordProgWithScore> getProgressions(Chord chord){
+        if (!content.keySet().contains(chord)){
+            return new ArrayList<>();
+        }
+        else{
+            return Collections.unmodifiableList(content.get(chord));
+        }
+    }
+    
+    /**
+     * Mutator.  Inserts a progression if its score is better than 
+     * the PROGRESSIONS_TO_TRACKth progression ending with the same 
+     * chord
+     * @param prog a ChordProgressionWithScore
+     */
+    public void addProgression(ChordProgWithScore progWithScore){
+        ChordProgression prog = progWithScore.getChordProg();
+        Chord last = prog.getLast();
+        Integer score = progWithScore.getScore();
+        
+        if (content.keySet().contains(last)){
+            List<ChordProgWithScore> progList = content.get(last);
+            int listSize = progList.size();
+            if (progList.get(listSize-1).getScore() <= score){
+                progList.add(progWithScore);
+            }
+            else{
+                int position = 0;
+                while (progList.get(position).getScore() <= score){
+                    position++;
+                }
+                progList.add(position, progWithScore);
+
+            }
+            if (progList.size() > PROGRESSIONS_TO_TRACK){
+                progList.remove(progList.size()-1);
+            }
+        }
+        else{
+            content.put(last, new ArrayList<>());
+            content.get(last).add(progWithScore);
+        }
+        checkRep();
+    }
+    
+    
+}
---
+++ test/solver/BestListTest.java
@@ -0,0 +1,103 @@
+package solver;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import chords.BasicChord;
+import chords.Chord;
+import chords.ChordProgression;
+import chords.ChordType;
+import chords.PrimitiveChord;
+import music.BasicNote;
+import music.Note;
+
+public class BestListTest {
+    
+    private static final BasicNote C = new BasicNote(0,0);
+    private static final BasicNote D = new BasicNote(1,2);
+    private static final BasicNote E = new BasicNote(2,4);
+    private static final BasicNote F = new BasicNote(3,5);
+    private static final BasicNote G = new BasicNote(4,7);
+    private static final BasicNote B = new BasicNote(6,11);
+
+    
+    private static final BasicChord G_DOM_SEVEN = new BasicChord(
+            B, D, F, G, new PrimitiveChord(G,ChordType.DOM7, 0));
+    private static final Chord G_DOM_SEVEN_REALIZED = new Chord(
+            new Note(B,4), new Note(D, 4), new Note(F, 3), 
+            new Note(G, 2),  G_DOM_SEVEN);
+    
+    private static final BasicChord C_MAJ = new BasicChord(
+            C, C, E, C, new PrimitiveChord(C, ChordType.MAJ, 0));
+    private static final Chord C_MAJ_REALIZED = new Chord(
+            new Note(C, 5), new Note(C, 4), new Note(E, 3), 
+            new Note(C, 3),  C_MAJ);
+    
+    private static final ChordProgression TEST_G_DOM7= 
+            ChordProgression.empty().append(G_DOM_SEVEN_REALIZED);
+    private static final ChordProgression TEST_PERFECT_CADENCE= 
+            ChordProgression.empty().append(G_DOM_SEVEN_REALIZED).append(C_MAJ_REALIZED);
+
+    private static final ChordProgWithScore TEST1 = new ChordProgWithScore(TEST_G_DOM7, 1);
+    private static final ChordProgWithScore TEST2 = new ChordProgWithScore(TEST_G_DOM7, 2);
+    private static final ChordProgWithScore TEST3 = new ChordProgWithScore(TEST_G_DOM7, 3);
+    private static final ChordProgWithScore TEST4 = new ChordProgWithScore(TEST_G_DOM7, 4);
+    
+    private static final ChordProgWithScore TEST2_PC = new ChordProgWithScore(TEST_PERFECT_CADENCE, 2);
+    private static final ChordProgWithScore TEST4_PC = new ChordProgWithScore(TEST_PERFECT_CADENCE, 4);
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false;
+    }
+
+    @Test
+    public void testBasic(){
+        BestList bestList = new BestList();
+        bestList.addProgression(TEST4);
+        assertEquals(1,bestList.getProgressions(G_DOM_SEVEN_REALIZED).size());
+        assertEquals(TEST4,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(0));
+    }
+    
+    @Test
+    public void testOrdering(){
+        BestList bestList = new BestList();
+        bestList.addProgression(TEST4);
+        bestList.addProgression(TEST2);
+        bestList.addProgression(TEST3);
+        assertEquals(3,bestList.getProgressions(G_DOM_SEVEN_REALIZED).size());
+        assertEquals(TEST2,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(0));
+        assertEquals(TEST3,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(1));
+        assertEquals(TEST4,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(2));
+    }
+    
+    @Test
+    public void testOverflow(){
+        BestList bestList = new BestList();
+        bestList.addProgression(TEST4);
+        bestList.addProgression(TEST2);
+        bestList.addProgression(TEST3);
+        bestList.addProgression(TEST1);
+        assertEquals(3,bestList.getProgressions(G_DOM_SEVEN_REALIZED).size());
+        assertEquals(TEST1,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(0));
+        assertEquals(TEST2,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(1));
+        assertEquals(TEST3,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(2));
+    }
+
+    @Test
+    public void testIndependence(){
+        BestList bestList = new BestList();
+        bestList.addProgression(TEST4);
+        bestList.addProgression(TEST3);
+        bestList.addProgression(TEST2_PC);
+        bestList.addProgression(TEST4_PC);
+        assertEquals(2,bestList.getProgressions(G_DOM_SEVEN_REALIZED).size());
+        assertEquals(2,bestList.getProgressions(C_MAJ_REALIZED).size());
+        assertEquals(TEST3,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(0));
+        assertEquals(TEST4,bestList.getProgressions(G_DOM_SEVEN_REALIZED).get(1));
+        assertEquals(TEST2_PC,bestList.getProgressions(C_MAJ_REALIZED).get(0));
+        assertEquals(TEST4_PC,bestList.getProgressions(C_MAJ_REALIZED).get(1));
+    }
+
+}
