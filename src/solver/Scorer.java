fbbb99e34ca01b83868806744322a73aa7e1406e
Brice Huang <bmhuang@mit.edu>
2016-11-10 18:11:55 -0500

    scorer method declarations

--- src/solver/Scorer.java
+++ src/solver/Scorer.java
@@ -24,9 +24,8 @@ public class Scorer {
      * No Directs
      * No melodic aug/dim intervals
      * No voice crossing
-     * Dom7 proper resolutions
-     * Dim7 proper resolutionss
-     * ii65 hold tonic
+     * Dom7, Dim7, proper resolutions
+     * ii65 hold tonic TODO
      * 
      * Last Transition:
      * 
@@ -38,6 +37,27 @@ public class Scorer {
     /*********************************
      * scoreChord and helper methods *
      *********************************/
+    
+    /**
+     * Scores the badness of a chord's doubling 
+     * @param chord current chord
+     * @param key key in which this chord should be analyzed
+     * @return score
+     */
+    private static Integer scoreDoubling(Chord chord, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+    
+    /**
+     * Scores the badness of voice overlapping
+     * @param chord current chord
+     * @param key key in which this chord should be analyzed
+     * @return score
+     */
+    private static Integer scoreVoiceOverlap(Chord chord, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
     
     /**
      * Scores the badness of a chord 
@@ -54,6 +74,72 @@ public class Scorer {
      **************************************/
 
     /**
+     * Scores for small movements
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreSmallMovement(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
+     * Scores for parallel perfect intervals
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreParallels(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
+     * Scores for direct perfect intervals
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreDirects(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
+     * Scores for bad melodic intervals
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreMelodicIntervals(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
+     * Scores for voice crossing
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreVoiceCrossing(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
+     * Scores for 7th chord resolutions
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreSevenChordResolutions(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
      * Scores the badness of a transition 
      * @param previous previous chord 
      * @param current current chord
@@ -67,6 +153,29 @@ public class Scorer {
     /****************************************************
      * scoreLastTransitionAdditional and helper methods *
      ****************************************************/
+    
+    /**
+     * Scores for everything moves stepwise in final cadence
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scoreEverythingStepwise(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
+     * Scores for PAC
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return score
+     */
+    private static Integer scorePAC(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
     
     /**
      * Additional score for last transition 
