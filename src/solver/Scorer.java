b4a0eebe399352582bfae2b8f5ad8aa5afc0522e
Brice Huang <bmhuang@mit.edu>
2016-11-10 14:26:15 -0500

    Added methods and specs for scorer, sequencer

--- src/solver/Scorer.java
+++ src/solver/Scorer.java
@@ -9,6 +9,16 @@ import music.Key;
 public class Scorer {
     
     /**
+     * Scores the badness of a chord 
+     * @param chord current chord
+     * @param key key in which this chord should be analyzed
+     * @return a score representing this chord's badness
+     */
+    public static Integer scoreChord(Chord chord, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    /**
      * Scores the badness of a transition 
      * @param previous previous chord 
      * @param current current chord
@@ -18,4 +28,17 @@ public class Scorer {
     public static Integer scoreTransition(Chord previous, Chord current, Key key){
         throw new RuntimeException("Unimplemented.");
     }
+    
+    /**
+     * Additional score for last transition 
+     * @param previous previous chord 
+     * @param current current chord
+     * @param key key in which this transition should be analyzed
+     * @return a score representing this transition's additional badness
+     */
+    public static Integer scoreLastTransitionAdditional(Chord previous, Chord current, Key key){
+        throw new RuntimeException("Unimplemented.");
+    }
+
+    
 }
--- src/solver/Sequencer.java
+++ src/solver/Sequencer.java
@@ -14,8 +14,54 @@ import music.Key;
 public class Sequencer {
     
     /**
+     * Sequences the first chord
+     * @param chordSet ways to sing the first chord
+     * @param key key in which to analyze the first chord
+     * @return a BestList of ways to sing the first chord, with scores
+     */
+    private static BestList sequenceFirst(Set<Chord> chordSet, Key key){
+        throw new RuntimeException("Unimplemented");
+    }
+    
+    /**
+     * Sequences the second through N-1th chords
+     * @param previousBest BestList of ways to sing the chords up to, but 
+     * excluding, this one, with scores
+     * @param chordSet ways to sing this chord
+     * @param key key in which to analyze this transition
+     * @return a BestList of ways to sing the chords up to, and including,
+     * this one, with scores
+     */
+    private static BestList sequenceMiddle(BestList previousBest, 
+            Set<Chord> chordSet, Key key){
+        throw new RuntimeException("Unimplemented");
+    }
+    
+    /**
+     * Sequences the last chord
+     * @param previousBest BestList of ways to sing the chords up to, but 
+     * excluding, this one, with scores
+     * @param chordSet ways to sing this chord
+     * @param key key in which to analyze this transition
+     * @return a BestList of ways to sing all the chords
+     */
+    private static BestList sequenceLast(BestList previousBest, 
+            Set<Chord> chordSet, Key key){
+        throw new RuntimeException("Unimplemented");
+    }
+
+    /**
+     * Finds the best chords from a BestList
+     * @param bestList a BestList
+     * @return a SortedFiniteProgList of the best chords from bestList
+     */
+    private static SortedFiniteProgList findBestProgs(BestList bestList){
+        throw new RuntimeException("Unimplemented")
+    }
+    
+    /**
      * Computes the best chord progression
-     * @param chordList a list of ways to sing chords. The ith item
+     * @param waysToSingChords a list of ways to sing chords. The ith item
      * is a set of all the ways to sing the ith chord.    
      * @param keys a list of keys, such that the ith item is the key
      * in which the ith chord should be considered.  We say that 
@@ -23,7 +69,7 @@ public class Sequencer {
      * @return The best way to sing this chord progression
      */
     public static ChordProgression findBestChordProgression(
-            List<Set<Chord>> chordList, List<Key> keys){
+            List<Set<Chord>> waysToSingChords, List<Key> keys){
         throw new RuntimeException("Unimplemented");
     }
 }
