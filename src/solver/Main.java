9f4337cc93740f6175d8ed9c47f3e868bd178f38
Brice Huang <bricehuang@dhcp-18-189-108-51.dyn.MIT.EDU>
2016-12-05 18:06:51 -0500

    Main module

---
+++ src/solver/Main.java
@@ -0,0 +1,18 @@
+package solver;
+
+import java.util.List;
+
+import chords.ChordProgression;
+
+/**
+ * Main module, which parses an input and finds the best chord progressions
+ * for it
+ */
+public class Main {
+    
+    public static List<ChordProgression> solve(String in){
+        // TODO
+        // pass to parser, then to chord generator, then to sequencer
+        throw new RuntimeException("Unimplemented.");
+    }
+}
