c8304a490fc30fc709bd39ba4dd949b023fadf6a
Brice Huang <bricehuang@dhcp-18-189-108-51.dyn.MIT.EDU>
2016-12-05 17:56:47 -0500

    Context tag implemented

---
+++ src/solver/ContextTag.java
@@ -0,0 +1,34 @@
+package solver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ContextTag {
+    APPLIED_DOMINANT, 
+    NEAPOLITAN, 
+    CADENTIAL_PREDOMINANT, // ii, ii65, iv, etc
+    CADENTIAL_I64,
+    CADENTIAL_V, // v or v7 right before i
+    CADENCE; // last note 
+    
+    private static Map<ContextTag, String> STRING_REPS;
+    static{
+        Map <ContextTag, String> tmpStringRep = new HashMap<>(); 
+        
+        tmpStringRep.put(APPLIED_DOMINANT, "Applied Dominant");
+        tmpStringRep.put(NEAPOLITAN, "Neapolitan");
+        tmpStringRep.put(CADENTIAL_PREDOMINANT, "Cadential Predominant");
+        tmpStringRep.put(CADENTIAL_I64, "Cadential I64");
+        tmpStringRep.put(CADENTIAL_V, "Cadential V");
+        tmpStringRep.put(CADENCE, "Cadence");
+    }
+    
+    /*******************
+     * Object Contract *
+     *******************/
+    
+    @Override
+    public String toString(){
+        return STRING_REPS.get(this);
+    }
+}
