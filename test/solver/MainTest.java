abfa0efb543ec253cea0acf4e874d2383c084783
Brice Huang <bricehuang@Brices-MBP.home>
2016-12-29 00:11:07 -0500

    minor edit

--- test/solver/MainTest.java
+++ test/solver/MainTest.java
@@ -9,26 +9,27 @@ public class MainTest {
         assert false;
     }
     
-    private static boolean REPORT = true;
-
+    private static final boolean REPORT = true;
+    private static final int MAX_REPORT = 1;
+    
     @Test
     public void testI64(){
         String input = "KEY:CMaj FMajT0 CMajT2 GMajT0 CMajT0Cad";
-        Main.solve(input, REPORT, 1);
+        Main.solve(input, REPORT, MAX_REPORT);
     }
-
     
     @Test
     public void testRealization1(){
         String input = "KEY:GMaj GMajT0 DDomS0 GMajT0 GDomS1\n"
                 + "CMajT0App DMajT1 GMajT0 AMinS0 GMajT2 DMajT0 GMajT0Cad";
-        Main.solve(input, REPORT, 1);
+        Main.solve(input, REPORT, MAX_REPORT);
     }
+    
     @Test
     public void testRealization2(){
         String input = "KEY:CMin CMinT0 A-MajT0 FMinT0 GDomS2\n"
                 + "KEY:B-Maj CMinT1App FMajT0 B-MajT1 CMinS1 FMajT0 B-MajT0Cad";
-        Main.solve(input, REPORT, 1);
+        Main.solve(input, REPORT, MAX_REPORT);
     }
 
 }
