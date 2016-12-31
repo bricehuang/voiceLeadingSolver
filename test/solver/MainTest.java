43059ad58b0892dce2d21108692dd8b0b45e6d2a
Brice Huang <bricehuang@Brices-MBP.home>
2016-12-31 02:37:18 -0500

    Main tests added. THIS ACTUALLY WORKS OMG

--- test/solver/MainTest.java
+++ test/solver/MainTest.java
@@ -1,6 +1,11 @@
 package solver;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import chords.ChordProgression;
+import player.ProgressionPlayer;
 
 public class MainTest {
     
@@ -15,21 +20,40 @@ public class MainTest {
     @Test
     public void testI64(){
         String input = "KEY:CMaj FMajT0 CMajT2 GMajT0 CMajT0Cad";
-        Main.solve(input, REPORT, MAX_REPORT);
+        ChordProgression best = Main.solve(input, REPORT, MAX_REPORT).get(0);
+        ProgressionPlayer.playProgression(best);
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     
     @Test
     public void testRealization1(){
         String input = "KEY:GMaj GMajT0 DDomS0 GMajT0 GDomS1App\n"
                 + "CMajT0 DMajT1 GMajT0 AMinS0 GMajT2 DMajT0 GMajT0Cad";
-        Main.solve(input, REPORT, MAX_REPORT);
+        ChordProgression best = Main.solve(input, REPORT, MAX_REPORT).get(0);
+        ProgressionPlayer.playProgression(best);
+        try {
+            Thread.sleep(22000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
     
     @Test
     public void testRealization2(){
         String input = "KEY:CMin CMinT0 A-MajT0 FMinT0 GDomS2App\n"
                 + "KEY:B-Maj CMinT1 FMajT0 B-MajT1 CMinS1 FMajT0 B-MajT0Cad";
-        Main.solve(input, REPORT, MAX_REPORT);
+        ChordProgression best = Main.solve(input, REPORT, MAX_REPORT).get(0);
+        ProgressionPlayer.playProgression(best);
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
