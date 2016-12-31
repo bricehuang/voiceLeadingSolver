01ce019a6eca9c0c9f885861f4fb2ef43f4dc244
Brice Huang <bricehuang@Brices-MBP.home>
2016-12-30 19:43:41 -0500

    Started ProgressionPlayer

---
+++ src/player/ProgressionPlayer.java
@@ -0,0 +1,17 @@
+package player;
+
+import chords.ChordProgression;
+
+/**
+ * A module that plays chord progressions
+ */
+public class ProgressionPlayer {
+    
+    /**
+     * Plays a chord progression
+     * @param progression
+     */
+    public static void playProgression(ChordProgression progression){
+        throw new RuntimeException("Unimplemented");
+    }
+}
