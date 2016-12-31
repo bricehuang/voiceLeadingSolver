c45d766d65f5f6986319aad6aad8bb64ed6f94d1
Brice Huang <bricehuang@Brices-MBP.home>
2016-12-31 12:54:30 -0500

    Solve-and-play methods implemented

--- src/solver/Main.java
+++ src/solver/Main.java
@@ -1,8 +1,12 @@
 package solver;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 import chords.ChordProgression;
+import player.ProgressionPlayer;
 import scorer.Scorer;
 
 /**
@@ -11,6 +15,17 @@ import scorer.Scorer;
  */
 public class Main {
     
+    private static boolean DEFAULT_REPORT = true;
+    private static int DEFAULT_MAXREPORT = 1;
+    
+    /**
+     * Finds the best chord progressions on a given input 
+     * @param in the input
+     * @param report if true, prints evaluation of best progressions
+     * @param maxReport maximum number of best progressions to report
+     * @return a List of top ChordProgressions of length at most
+     * SortedFiniteProgList.PROGRESSIONS_TO_TRACK
+     */
     public static List<ChordProgression> solve(String in, boolean report, int maxReport){
         ParseResult parsedInput = Parser.parse(in);
         List<ChordProgression> bestProgressions = Solver.solve(
@@ -31,5 +46,28 @@ public class Main {
         return bestProgressions;
     }
     
-    // TODO: solve and play
+    /**
+     * Finds the best chord progression on a given input and plays it
+     * @param in the input
+     */
+    public static void solveAndPlay(String in){
+        ChordProgression best = solve(in, DEFAULT_REPORT, DEFAULT_MAXREPORT).get(0);  
+        ProgressionPlayer.playProgression(best); 
+    }
+
+    /**
+     * Finds the best chord progression on a given input and plays it
+     * @param filename the file containing the input
+     * @throws IOException 
+     */
+    public static void solveAndPlayFile(String filename) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String in = "";
+        for(String line = reader.readLine(); line != null; line = reader.readLine()){
+            in += (line + "\n");
+        }
+        reader.close();
+        solveAndPlay(in);
+    }
+
 }
