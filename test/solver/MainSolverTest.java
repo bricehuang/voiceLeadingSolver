a6745c964c2172f6717346bf6f3841c1ad4d035d
Brice Huang <bricehuang@Brices-MBP.home>
2016-12-31 13:08:50 -0500

    Took playing out of Main Solver Test

--- test/solver/MainSolverTest.java
+++ test/solver/MainSolverTest.java
@@ -21,7 +21,6 @@ public class MainSolverTest {
     public void testI64(){
         String input = "KEY:CMaj FMajT0 CMajT2 GMajT0 CMajT0Cad";
         ChordProgression best = Main.solve(input, REPORT, MAX_REPORT).get(0);
-        ProgressionPlayer.playProgression(best);
         try {
             Thread.sleep(8000);
         } catch (InterruptedException e) {
@@ -34,7 +33,6 @@ public class MainSolverTest {
         String input = "KEY:GMaj GMajT0 DDomS0 GMajT0 GDomS1App\n"
                 + "CMajT0 DMajT1 GMajT0 AMinS0 GMajT2 DMajT0 GMajT0Cad";
         ChordProgression best = Main.solve(input, REPORT, MAX_REPORT).get(0);
-        ProgressionPlayer.playProgression(best);
         try {
             Thread.sleep(22000);
         } catch (InterruptedException e) {
@@ -48,7 +46,6 @@ public class MainSolverTest {
         String input = "KEY:CMin CMinT0 A-MajT0 FMinT0 GDomS2App\n"
                 + "KEY:B-Maj CMinT1 FMajT0 B-MajT1 CMinS1 FMajT0 B-MajT0Cad";
         ChordProgression best = Main.solve(input, REPORT, MAX_REPORT).get(0);
-        ProgressionPlayer.playProgression(best);
         try {
             Thread.sleep(20000);
         } catch (InterruptedException e) {
