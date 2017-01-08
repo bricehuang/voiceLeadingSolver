b8063704d88c8e8ab1f3493fecc6855372386816
Brice Huang <bricehuang@Brices-MacBook-Pro.local>
2017-01-07 23:15:45 -0500

    Neopolitan 6th parser test

--- test/solver/ParserTest.java
+++ test/solver/ParserTest.java
@@ -22,9 +22,13 @@ public class ParserTest {
     
     private static final BasicNote C = new BasicNote(0,0);
     private static final BasicNote D = new BasicNote(1,2);
+    private static final BasicNote E = new BasicNote(2,4);
     private static final BasicNote F = new BasicNote(3,5);
     private static final BasicNote G = new BasicNote(4,7);
+    private static final BasicNote A = new BasicNote(5,9);
+    private static final BasicNote Bb = new BasicNote(6,10);
     private static final Key C_MAJOR = new Key(C,true);
+    private static final Key A_MAJOR = new Key(A,true);
     
     @Test
     public void cadenceTest(){
@@ -93,6 +97,30 @@ public class ParserTest {
         assertTrue(contextTags.get(2).contains(ContextTag.CADENTIAL_V));
         assertEquals(1, contextTags.get(3).size());
         assertTrue(contextTags.get(3).contains(ContextTag.CADENCE));
+    }
+    
+    @Test
+    public void TestNeapolitan(){
+        ParseResult parse = Parser.parse("KEY:AMaj B-MajT1Nea EMajT0 AMajT0Cad");
+        List<PrimitiveChord> primitiveChords = parse.getPrimitiveChords();
+        List<Key> keys = parse.getKeys();
+        List<Set<ContextTag>> contextTags = parse.getContextTags();
+        assertEquals(3, primitiveChords.size());
+        assertEquals(3, keys.size());
+        assertEquals(3, contextTags.size());
+        assertEquals(new PrimitiveChord(Bb, ChordType.MAJ, 1), primitiveChords.get(0));
+        assertEquals(new PrimitiveChord(E, ChordType.MAJ, 0), primitiveChords.get(1));
+        assertEquals(new PrimitiveChord(A, ChordType.MAJ, 0), primitiveChords.get(2));
+        assertEquals(A_MAJOR, keys.get(0));
+        assertEquals(A_MAJOR, keys.get(1));
+        assertEquals(A_MAJOR, keys.get(2));
+        assertEquals(2, contextTags.get(0).size());
+        assertTrue(contextTags.get(0).contains(ContextTag.NEAPOLITAN));
+        assertTrue(contextTags.get(0).contains(ContextTag.CADENTIAL_PREDOMINANT));
+        assertEquals(1, contextTags.get(1).size());
+        assertTrue(contextTags.get(1).contains(ContextTag.CADENTIAL_V));
+        assertEquals(1, contextTags.get(2).size());
+        assertTrue(contextTags.get(2).contains(ContextTag.CADENCE));
     }
 
 }
