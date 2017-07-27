46533c23e6259a71ef95f6f9cb290708335136e4
Brice Huang <bhuang@dropbox.com>
2017-07-26 22:41:22 -0700

    don't run MainTest every time

--- test/solver/MainTest.java
+++ test/solver/MainTest.java
@@ -8,6 +8,7 @@ public class MainTest {
         Main.main(new String[]{"sample_satb/" + filename});
     }
     
+    private static final boolean PLAY_REALIZATIONS = false;
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
@@ -16,46 +17,54 @@ public class MainTest {
     
     @Test
     public void ii65i64viTest(){
-        playSampleSATB("ii65-i64-v-i-cadence.satb");
-        try{
-            Thread.sleep(8000);
-        }
-        catch(InterruptedException e){
-            e.printStackTrace();
-        }
+    		if (PLAY_REALIZATIONS){
+	        playSampleSATB("ii65-i64-v-i-cadence.satb");
+	        try{
+	            Thread.sleep(8000);
+	        }
+	        catch(InterruptedException e){
+	            e.printStackTrace();
+	        }
+    		}
     }
 
     @Test
     public void realization1Test(){
-        playSampleSATB("realization1.satb");
-        try{
-            Thread.sleep(22000);
-        }
-        catch(InterruptedException e){
-            e.printStackTrace();
-        }
+    		if (PLAY_REALIZATIONS) {
+    	        playSampleSATB("realization1.satb");
+    	        try{
+    	            Thread.sleep(22000);
+    	        }
+    	        catch(InterruptedException e){
+    	            e.printStackTrace();
+    	        }    			
+    		}
     }
 
     @Test
     public void realization2Test(){
-        playSampleSATB("realization2.satb");
-        try{
-            Thread.sleep(20000);
-        }
-        catch(InterruptedException e){
-            e.printStackTrace();
-        }
+    		if (PLAY_REALIZATIONS) {
+    	        playSampleSATB("realization2.satb");
+    	        try{
+    	            Thread.sleep(20000);
+    	        }
+    	        catch(InterruptedException e){
+    	            e.printStackTrace();
+    	        }    			
+    		}
     }
     
     @Test
     public void realization3Test(){
-        playSampleSATB("realization3.satb");
-        try{
-            Thread.sleep(20000);
-        }
-        catch(InterruptedException e){
-            e.printStackTrace();
-        }
+    		if (PLAY_REALIZATIONS) {    			
+    	        playSampleSATB("realization3.satb");
+    	        try{
+    	            Thread.sleep(20000);
+    	        }
+    	        catch(InterruptedException e){
+    	            e.printStackTrace();
+    	        }
+    		}
     }
 
 }
