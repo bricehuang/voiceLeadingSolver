40d68daa2075c321aa1f90b0ec325b94db55e189
Brice Huang <bhuang@dropbox.com>
2017-07-30 22:45:30 -0700

    BasicChordTest

--- test/chords/BasicChordTest.java
+++ test/chords/BasicChordTest.java
@@ -4,30 +4,23 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import music.BasicNote;
-import music.Key;
+import test_framework.MusicTestFramework;
 
-public class BasicChordTest {
+public class BasicChordTest extends MusicTestFramework {
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false;
     }
-
-    private static final BasicNote C = new BasicNote(0,0);
-    private static final BasicNote D = new BasicNote(1,2);
-    private static final BasicNote E = new BasicNote(2,4);
-    private static final BasicNote Eb = new BasicNote(2,3);
-    private static final BasicNote F = new BasicNote(3,5);
-    private static final BasicNote G = new BasicNote(4,7);
-    private static final BasicNote B = new BasicNote(6,11);
     
     private static final BasicChord G_DOM_SEVEN = new BasicChord(
-            B, D, F, G, new PrimitiveChord(G,ChordType.DOM7, 0));
+        B, D, F, G, G_DOM7_ROOT
+    );
     private static final BasicChord C_MAJ = new BasicChord(
-            C, C, E, C, new PrimitiveChord(C, ChordType.MAJ, 0));
+        C, C, E, C, C_MAJ_ROOT
+    );
     private static final BasicChord C_MIN = new BasicChord(
-            C, C, Eb, C, new PrimitiveChord(C, ChordType.MIN, 0));
-    private static final Key C_MINOR = new Key(C, false);
+        C, C, Eb, C, C_MIN_ROOT
+    );
     
     @Test
     public void toStringTest(){
---
+++ test/test_framework/MusicTestFramework.java
@@ -0,0 +1,30 @@
+package test_framework;
+
+import chords.ChordType;
+import chords.PrimitiveChord;
+import music.BasicNote;
+import music.Key;
+
+public class MusicTestFramework {
+    
+    // BasicNote 
+    public static final BasicNote C = new BasicNote(0,0);
+    public static final BasicNote D = new BasicNote(1,2);
+    public static final BasicNote E = new BasicNote(2,4);
+    public static final BasicNote Eb = new BasicNote(2,3);
+    public static final BasicNote F = new BasicNote(3,5);
+    public static final BasicNote G = new BasicNote(4,7);
+    public static final BasicNote B = new BasicNote(6,11);
+    
+    // Key
+    public static final Key C_MINOR = new Key(C, false);
+    
+    // PrimitiveChord
+    public static final PrimitiveChord C_MAJ_ROOT 
+        = new PrimitiveChord(C, ChordType.MAJ, 0);
+    public static final PrimitiveChord C_MIN_ROOT
+        = new PrimitiveChord(C, ChordType.MIN, 0);
+    public static final PrimitiveChord G_DOM7_ROOT = 
+        new PrimitiveChord(G,ChordType.DOM7, 0);
+
+}
