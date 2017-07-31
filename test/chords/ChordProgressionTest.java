14a4b1029fd678aad8290fc2ae5bd7004f33a6ba
Brice Huang <bhuang@dropbox.com>
2017-07-30 22:53:53 -0700

    ChordProgressionTest (oops this was deprecated)

--- test/chords/ChordProgressionTest.java
+++ test/chords/ChordProgressionTest.java
@@ -4,39 +4,24 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import music.BasicNote;
-import music.Note;
+import test_framework.MusicTestFramework;
 
-public class ChordProgressionTest {
+public class ChordProgressionTest extends MusicTestFramework {
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false;
     }
-    
-    private static final BasicNote C = new BasicNote(0,0);
-    private static final BasicNote D = new BasicNote(1,2);
-    private static final BasicNote E = new BasicNote(2,4);
-    private static final BasicNote G = new BasicNote(4,7);
-    private static final BasicNote B = new BasicNote(6,11);
-    
-    private static final PrimitiveChord C_MAJ_64 = 
-            new PrimitiveChord(C, ChordType.MAJ, 2);
+        
     private static final Chord C_MAJ_64_REALIZED = new Chord(
-            new Note(C, 5), new Note(E, 4), new Note(G, 3), 
-            new Note(G, 2),  C_MAJ_64);
-    
-    private static final PrimitiveChord G_MAJ_ROOT = 
-            new PrimitiveChord(G,ChordType.MAJ, 0);
+        C5, E4, G3, G2, C_MAJ_64
+    );
     private static final Chord G_MAJ_REALIZED = new Chord(
-            new Note(B,4), new Note(D, 4), new Note(G, 3), 
-            new Note(G, 2),  G_MAJ_ROOT);
-            
-    private static final PrimitiveChord C_MAJ_ROOT = 
-            new PrimitiveChord(C, ChordType.MAJ, 0);
+        B4, D4, G3, G2, G_MAJ_ROOT
+    );
     private static final Chord C_MAJ_REALIZED = new Chord(
-            new Note(C, 5), new Note(E, 4), new Note(G, 3), 
-            new Note(C, 3),  C_MAJ_ROOT);
-
+        C5, E4, G3, C3, C_MAJ_ROOT
+    ); 
+    
     @Test
     public void emptyTest(){
         ChordProgressionDeprecated empty = ChordProgressionDeprecated.empty();
@@ -47,8 +32,7 @@ public class ChordProgressionTest {
     @Test
     public void oneChordTest(){
         ChordProgressionDeprecated empty = ChordProgressionDeprecated.empty();
-        ChordProgressionDeprecated oneChord = 
-                empty.append(C_MAJ_REALIZED);
+        ChordProgressionDeprecated oneChord = empty.append(C_MAJ_REALIZED);
         assertEquals(1, oneChord.length());
         assertEquals("\n[C5|E4|G3|C3]", oneChord.toString());
     }
--- test/test_framework/MusicTestFramework.java
+++ test/test_framework/MusicTestFramework.java
@@ -4,6 +4,7 @@ import chords.ChordType;
 import chords.PrimitiveChord;
 import music.BasicNote;
 import music.Key;
+import music.Note;
 
 public class MusicTestFramework {
     
@@ -16,15 +17,30 @@ public class MusicTestFramework {
     public static final BasicNote G = new BasicNote(4,7);
     public static final BasicNote B = new BasicNote(6,11);
     
+    // Note
+    public static final Note C5 = new Note(C, 5); 
+    public static final Note B4 = new Note(B, 4); 
+    public static final Note E4 = new Note(E, 4); 
+    public static final Note D4 = new Note(D, 4); 
+    public static final Note G3 = new Note(G, 3); 
+    public static final Note C3 = new Note(C, 3);  
+    public static final Note G2 = new Note(G, 2);   
+
+
+    
     // Key
     public static final Key C_MINOR = new Key(C, false);
     
     // PrimitiveChord
     public static final PrimitiveChord C_MAJ_ROOT 
         = new PrimitiveChord(C, ChordType.MAJ, 0);
+    public static final PrimitiveChord C_MAJ_64
+        = new PrimitiveChord(C, ChordType.MAJ, 2);
     public static final PrimitiveChord C_MIN_ROOT
         = new PrimitiveChord(C, ChordType.MIN, 0);
-    public static final PrimitiveChord G_DOM7_ROOT = 
-        new PrimitiveChord(G,ChordType.DOM7, 0);
+    public static final PrimitiveChord G_MAJ_ROOT  
+        = new PrimitiveChord(G, ChordType.MAJ, 0);
+    public static final PrimitiveChord G_DOM7_ROOT  
+        = new PrimitiveChord(G,ChordType.DOM7, 0);
 
 }
