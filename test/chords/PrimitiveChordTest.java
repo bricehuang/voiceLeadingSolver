76e1194e0bcc771f72919296d1ceffd72b2aca94
Brice Huang <bhuang@dropbox.com>
2017-07-30 23:08:32 -0700

    PrimitiveChordTest

--- test/chords/PrimitiveChordTest.java
+++ test/chords/PrimitiveChordTest.java
@@ -8,9 +8,10 @@ import java.util.List;
 import org.junit.Test;
 
 import music.BasicNote;
+import test_framework.MusicTestFramework;
 
 
-public class PrimitiveChordTest {
+public class PrimitiveChordTest extends MusicTestFramework {
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false;
@@ -18,57 +19,29 @@ public class PrimitiveChordTest {
 
     @Test
     public void noteSetTest(){
-        List<BasicNote> cMajorTriad = Arrays.asList(
-                new BasicNote(0,0), new BasicNote(2,4), new BasicNote(4,7)
-                );
-        assertEquals(cMajorTriad, new PrimitiveChord(
-                new BasicNote(0,0), ChordType.MAJ, 0).noteList());
+        List<BasicNote> cMajorTriad = Arrays.asList(C, E, G);
+        assertEquals(cMajorTriad, C_MAJ_ROOT.noteList());
 
-        List<BasicNote> eMinorTriad = Arrays.asList(
-                new BasicNote(2,4), new BasicNote(4,7), new BasicNote(6,11)
-                );
-        assertEquals(eMinorTriad, new PrimitiveChord(
-                new BasicNote(2,4), ChordType.MIN, 0).noteList());
+        List<BasicNote> eMinorTriad = Arrays.asList(E, G, B);
+        assertEquals(eMinorTriad, E_MIN_ROOT.noteList());
 
-        List<BasicNote> gDomSeven = Arrays.asList(
-                new BasicNote(4,7), new BasicNote(6,11), 
-                new BasicNote(1,2), new BasicNote(3,5)
-                );
-        assertEquals(gDomSeven, new PrimitiveChord(
-                new BasicNote(4,7), ChordType.DOM7, 0).noteList());
+        List<BasicNote> gDomSeven = Arrays.asList(G, B, D, F);
+        assertEquals(gDomSeven, G_DOM7_ROOT.noteList());
 
-        List<BasicNote> dbMajSeven = Arrays.asList(
-                new BasicNote(1,1), new BasicNote(3,5), 
-                new BasicNote(5,8), new BasicNote(0,0)
-                );
-        assertEquals(dbMajSeven, new PrimitiveChord(
-                new BasicNote(1,1), ChordType.MAJ7, 0).noteList());
+        List<BasicNote> dbMajSeven = Arrays.asList(Db, F, Ab, C);
+        assertEquals(dbMajSeven, Db_MAJ7_ROOT.noteList());
 
-        List<BasicNote> aMinSeven = Arrays.asList(
-                new BasicNote(5,9), new BasicNote(0,0), 
-                new BasicNote(2,4), new BasicNote(4,7)
-                );
-        assertEquals(aMinSeven, new PrimitiveChord(
-                new BasicNote(5,9), ChordType.MIN7, 0).noteList());
+        List<BasicNote> aMinSeven = Arrays.asList(A, C, E, G);
+        assertEquals(aMinSeven, A_MIN7_ROOT.noteList());
 
-        List<BasicNote> cshDimSeven = Arrays.asList(
-                new BasicNote(0,1), new BasicNote(2,4), 
-                new BasicNote(4,7), new BasicNote(6,10)
-                );
-        assertEquals(cshDimSeven, new PrimitiveChord(
-                new BasicNote(0,1), ChordType.DIM7, 0).noteList());
+        List<BasicNote> cshDimSeven = Arrays.asList(Cs, E, G, Bb);
+        assertEquals(cshDimSeven, Cs_DIM7_ROOT.noteList());
     }
     
     @Test
     public void toStringTest(){
-        assertEquals("C MAJOR INVERSION 0", 
-                new PrimitiveChord(new BasicNote(0,0), 
-                        ChordType.MAJ, 0).toString());
-        assertEquals("G MINOR INVERSION 2", 
-                new PrimitiveChord(new BasicNote(4,7), 
-                        ChordType.MIN, 2).toString());
-        assertEquals("E- DOMINANT 7TH INVERSION 3", 
-                new PrimitiveChord(new BasicNote(2,3), 
-                        ChordType.DOM7, 3).toString());
+        assertEquals("C MAJOR INVERSION 0", C_MAJ_ROOT.toString());
+        assertEquals("G MINOR INVERSION 2", G_MIN_64.toString());
+        assertEquals("E- DOMINANT 7TH INVERSION 3", Eb_DOM7_42.toString()); 
     }
 }
--- test/test_framework/MusicTestFramework.java
+++ test/test_framework/MusicTestFramework.java
@@ -10,11 +10,16 @@ public class MusicTestFramework {
     
     // BasicNote 
     public static final BasicNote C = new BasicNote(0,0);
+    public static final BasicNote Cs = new BasicNote(0,1);
+    public static final BasicNote Db = new BasicNote(1,1);
     public static final BasicNote D = new BasicNote(1,2);
     public static final BasicNote E = new BasicNote(2,4);
     public static final BasicNote Eb = new BasicNote(2,3);
     public static final BasicNote F = new BasicNote(3,5);
     public static final BasicNote G = new BasicNote(4,7);
+    public static final BasicNote Ab = new BasicNote(5,8);
+    public static final BasicNote A = new BasicNote(5,9);
+    public static final BasicNote Bb = new BasicNote(6,10);
     public static final BasicNote B = new BasicNote(6,11);
     
     // Note
@@ -41,9 +46,21 @@ public class MusicTestFramework {
         = new PrimitiveChord(C, ChordType.MAJ, 2);
     public static final PrimitiveChord C_MIN_ROOT
         = new PrimitiveChord(C, ChordType.MIN, 0);
+    public static final PrimitiveChord Cs_DIM7_ROOT
+        = new PrimitiveChord(Cs, ChordType.DIM7, 0);
+    public static final PrimitiveChord Db_MAJ7_ROOT 
+        = new PrimitiveChord(Db, ChordType.MAJ7, 0);
+    public static final PrimitiveChord Eb_DOM7_42
+        = new PrimitiveChord(Eb, ChordType.DOM7, 3);
+    public static final PrimitiveChord E_MIN_ROOT
+        = new PrimitiveChord(E, ChordType.MIN, 0);
     public static final PrimitiveChord G_MAJ_ROOT  
         = new PrimitiveChord(G, ChordType.MAJ, 0);
+    public static final PrimitiveChord G_MIN_64  
+        = new PrimitiveChord(G, ChordType.MIN, 2);
     public static final PrimitiveChord G_DOM7_ROOT  
-        = new PrimitiveChord(G,ChordType.DOM7, 0);
+        = new PrimitiveChord(G, ChordType.DOM7, 0);
+    public static final PrimitiveChord A_MIN7_ROOT  
+        = new PrimitiveChord(A, ChordType.MIN7, 0);
 
 }
