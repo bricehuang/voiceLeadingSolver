08c1af62a2a4110d3ad20ec3c5244695dda8f6d8
Brice Huang <bricehuang@Brices-MacBook-Pro.local>
2017-08-05 20:57:24 -0700

    Parallels scorer

--- src/scorer_transitions/ParallelsScorer.java
+++ src/scorer_transitions/ParallelsScorer.java
@@ -1,13 +1,66 @@
 package scorer_transitions;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import chord_data.ChordWithContext;
+import music.BasicInterval; 
+import music.BasicNote;
+import score_data.TransitionPenaltyType;
 import score_data.TransitionScoreNew;
+import solver.SolverUtils;
 
 public class ParallelsScorer implements TransitionScorer {
+    
+    private static final BasicInterval UNISON = new BasicInterval(0,0);
+    private static final BasicInterval PERFECT_FIFTH = new BasicInterval(4,7);
+    private static final Set<BasicInterval> PERFECT_INTERVALS = 
+        Collections.unmodifiableSet(
+            new HashSet<>(Arrays.asList(UNISON, PERFECT_FIFTH))
+        );
+
+    private boolean isParallelPerfectInterval(
+        BasicNote previousLower,
+        BasicNote previousUpper,
+        BasicNote currentLower,
+        BasicNote currentUpper
+    ){
+        BasicInterval previousInterval = BasicInterval.intervalBetween(
+            previousLower, previousUpper
+        );
+        BasicInterval currentInterval = BasicInterval.intervalBetween(
+            currentLower, currentUpper
+        );
+        return ( previousInterval.equals(currentInterval) && 
+                 PERFECT_INTERVALS.contains(currentInterval) && 
+                 ! previousLower.equals(currentLower) );
+    }
 
     @Override
-    public TransitionScoreNew scoreTransition(ChordWithContext previous, ChordWithContext current) {
-        throw new RuntimeException("Unimplemented");
+    public TransitionScoreNew scoreTransition(
+        ChordWithContext previous, ChordWithContext current
+    ) {
+        TransitionScoreNew score = new TransitionScoreNew();
+        List<BasicNote> previousNotes = SolverUtils.spellBasicNotes(
+            previous.getChord()
+        );
+        List<BasicNote> currentNotes = SolverUtils.spellBasicNotes(
+            current.getChord()
+        );
+        for (int upper=0; upper<3; upper++) {
+            for (int lower=upper+1; lower<4; lower++) {
+                if (isParallelPerfectInterval(
+                    previousNotes.get(lower), previousNotes.get(upper),
+                    currentNotes.get(lower), currentNotes.get(upper)
+                )){
+                    score.addPenalty(TransitionPenaltyType.PARALLEL);
+                }
+            }
+        }
+        return score;
     }
 
 }
---
+++ test/scorer_transitions/ParallelsScorerTest.java
@@ -0,0 +1,70 @@
+package scorer_transitions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import chord_data.ChordWithContext;
+import score_data.TransitionPenaltyType;
+import test_framework.MusicTestFramework;
+
+public class ParallelsScorerTest extends MusicTestFramework {
+    
+    private Map<TransitionPenaltyType, Integer> computePenalties(
+        ChordWithContext previous, ChordWithContext current
+    ) {
+        return computeTransitionPenalties(
+            new ParallelsScorer(), previous, current
+        );
+    }
+
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false;   
+    }
+    
+    @Test
+    public void testParallelsGood(){
+        ChordWithContext previous = makeChordWithContext(
+            C5, G4, E4, G3, C_MAJ_64, C_MAJOR, NO_CONTEXTS
+        );
+        ChordWithContext current = makeChordWithContext(
+            B4, F4, D4, G3, G_DOM7_ROOT, C_MAJOR, NO_CONTEXTS
+        );
+        Map<TransitionPenaltyType, Integer> penaltyCount = 
+            computePenalties(previous, current);        
+        assertEquals(0, penaltyCount.keySet().size());
+    }
+
+    @Test
+    public void testParallelsBad(){
+        ChordWithContext previous = makeChordWithContext(
+            F5, A4, D4, F3, D_MIN_6, C_MAJOR, NO_CONTEXTS
+        );
+        ChordWithContext current = makeChordWithContext(
+            E5, G4, C4, C3, C_MAJ_ROOT, C_MAJOR, NO_CONTEXTS
+        );
+        Map<TransitionPenaltyType, Integer> penaltyCount = 
+            computePenalties(previous, current);        
+        assertEquals(1, penaltyCount.keySet().size());
+        assertTrue(penaltyCount.get(PARALLEL) == 1);
+    }
+    
+    @Test
+    public void testParallelsExtraBad(){
+        ChordWithContext previous = makeChordWithContext(
+            F5, A4, D4, D3, D_MIN_ROOT, C_MAJOR, NO_CONTEXTS
+        );
+        ChordWithContext current = makeChordWithContext(
+            E5, G4, C4, C3, C_MAJ_ROOT, C_MAJOR, NO_CONTEXTS
+        );
+        Map<TransitionPenaltyType, Integer> penaltyCount = 
+            computePenalties(previous, current);        
+        assertEquals(1, penaltyCount.keySet().size());
+        assertTrue(penaltyCount.get(PARALLEL) == 3);
+    }
+
+}
